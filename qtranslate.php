cfd20c776278ffae2a06c48ba2bd796fd8e42390
chineseleper <chineseleper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-03-01 12:03:27 +0000

    fixed time display bug, added option to display image+text and dropdown box
    git-svn-id: http://plugins.svn.wordpress.org/qtranslate/trunk@33438 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- qtranslate.php
+++ qtranslate.php
@@ -3,7 +3,7 @@
 Plugin Name: qTranslate
 Plugin URI: http://www.qianqin.de/qtranslate/
 Description: Adds userfriendly multilingual content support into Wordpress. Inspired by <a href="http://fredfred.net/skriker/index.php/polyglot">Polyglot</a> from Martin Chlupac.
-Version: 1.0 beta 6
+Version: 1.0 beta 7
 Author: Qian Qin
 Author URI: http://www.qianqin.de
 Tags: multilingual, multi, language, admin, tinymce, qTranslate, Polyglot, bilingual, widget
@@ -471,6 +471,22 @@ function qtrans_insertCategoryInput($language){
         ";
     return $html;    
 }
+function qtrans_insertDropDownElement($language, $url, $id){
+    global $q_config;
+    $html ="
+        var sb = document.getElementById('qtrans_select_".$id."');
+        var o = document.createElement('option');
+        var l = document.createTextNode('".$q_config['language_name'][$language]."');
+        ";
+    if($q_config['language']==$language)
+        $html .= "o.selected = 'selected';";
+    $html .= "
+        o.value = '".$url."';
+        o.appendChild(l);
+        sb.appendChild(o);
+        ";
+    return $html;    
+}
 
 function qtrans_modifyUpload() {
     global $q_config;
@@ -886,6 +902,7 @@ function qtrans_header(){
     echo "<style type=\"text/css\" media=\"screen\">\n";
     echo ".qtrans_flag span { display:none }\n";
     echo ".qtrans_flag { height:12px; width:18px; display:block }\n";
+    echo ".qtrans_flag_and_text { padding-left:20px }\n";
     foreach($q_config['enabled_languages'] as $language) {
         echo ".qtrans_flag_".$language." { background:url(".get_option('home').'/'.$q_config['flag_location'].$q_config['flag'][$language].") no-repeat }\n";
     }    
@@ -976,8 +993,8 @@ function qtrans_time($time, $default = '', $format ='') {
     if($format=='') return $default;
     // return translated date
     if($q_config['use_strftime'])
-        return $before.qtrans_strftime($format, $date).$after;
-    return $before.date($format, $date).$after;
+        return $before.qtrans_strftime($format, $time).$after;
+    return $before.date($format, $time).$after;
 }
 
 function qtrans_timeFromCommentForCurrentLanguage($old_date, $format = '', $gmt = false) {
@@ -1010,19 +1027,54 @@ function qtrans_timeFromPostForCurrentLanguage($old_date, $format = '', $gmt = f
 /* END TIME FUNCTIONS */
 
 // Language Select Code for non-Widget users
-function qtrans_generateLanguageSelectCode($useflags=false) {
+function qtrans_generateLanguageSelectCode($style='', $id='qtrans_language_chooser') {
     global $q_config;
-    echo "<ul class=\"qtrans_language_chooser\">";
-    foreach($q_config['enabled_languages'] as $language) {
-        echo '<li';
-        if($lanuage == $q_config[$lanuage])
-            echo ' class="active"';
-        echo '><a href="'.qtrans_convertURL($_SERVER['REQUEST_URI'], $language).'"';
-        if($useflags)
-            echo ' class="qtrans_flag qtrans_flag_'.$language.'"';
-        echo '><span>'.$q_config['language_name'][$language].'</span></a></li>';
+    if($style=='') $style='text';
+    if(is_bool($style)&&$style) $style='image';
+    switch($style) {
+        case 'image':
+        case 'text':
+        case 'dropdown':
+            echo '<ul class="qtrans_language_chooser" id="'.$id.'">';
+            foreach($q_config['enabled_languages'] as $language) {
+                echo '<li';
+                if($lanuage == $q_config[$lanuage])
+                    echo ' class="active"';
+                echo '><a href="'.qtrans_convertURL($_SERVER['REQUEST_URI'], $language).'"';
+                if($style=='image')
+                    echo ' class="qtrans_flag qtrans_flag_'.$language.'"';
+                echo '><span>'.$q_config['language_name'][$language].'</span></a></li>';
+            }
+            echo "</ul><div class=\"qtrans_widget_end\"></div>";
+            if($style=='dropdown') {
+                echo "<script type=\"text/javascript\">\n// <![CDATA[\r\n";
+                echo "var lc = document.getElementById('".$id."');\n";
+                echo "var s = document.createElement('select');\n";
+                echo "s.id = 'qtrans_select_".$id."';\n";
+                echo "lc.parentNode.insertBefore(s,lc);";
+                // create dropdown fields for each language
+                foreach($q_config['enabled_languages'] as $language) {
+                    echo qtrans_insertDropDownElement($language,qtrans_convertURL($_SERVER['REQUEST_URI'], $language),$id);
+                }
+                // hide html language chooser text
+                echo "s.onchange = function() { document.location.href = this.value;}\n";
+                echo "lc.style.display='none';\n";
+                echo "// ]]>\n</script>\n";
+            }
+            break;
+        case 'both':
+            echo '<ul class="qtrans_language_chooser" id="'.$id.'">';
+            foreach($q_config['enabled_languages'] as $language) {
+                echo '<li';
+                if($lanuage == $q_config[$lanuage])
+                    echo ' class="active"';
+                echo '><a href="'.qtrans_convertURL($_SERVER['REQUEST_URI'], $language).'"';
+                echo ' class="qtrans_flag_'.$language.' qtrans_flag_and_text"';
+                echo '><span>'.$q_config['language_name'][$language].'</span></a></li>';
+            }
+            echo "</ul><div class=\"qtrans_widget_end\"></div>";
+            break;
     }
-    echo "</ul><div class=\"qtrans_widget_end\"></div>";
 }
 
 /* BEGIN WIDGETS */
@@ -1045,7 +1097,7 @@ function qtrans_widget_init() {
         echo $before_widget;
         if($options['qtrans-switch-hide-title']!='on')
             echo $before_title . $title . $after_title;
-        qtrans_generateLanguageSelectCode($options['qtrans-switch-use-flags']=='on');
+        qtrans_generateLanguageSelectCode($options['qtrans-switch-type']);
         echo $after_widget;     
     }
     
@@ -1058,21 +1110,26 @@ function qtrans_widget_init() {
             // Clean up control form submission options
             $options['qtrans-switch-title'] = strip_tags(stripslashes($_POST['qtrans-switch-title']));
             $options['qtrans-switch-hide-title'] = strip_tags(stripslashes($_POST['qtrans-switch-hide-title']));
-            $options['qtrans-switch-use-flags'] = strip_tags(stripslashes($_POST['qtrans-switch-use-flags']));
+            $options['qtrans-switch-type'] = strip_tags(stripslashes($_POST['qtrans-switch-type']));
             update_option('qtranslate_switch', $options);
         }
 
         // Format options as valid HTML. Hey, why not.
         $title = htmlspecialchars($options['qtrans-switch-title'], ENT_QUOTES);
         $hide_title = htmlspecialchars($options['qtrans-switch-hide-title'], ENT_QUOTES);
-        $use_flags = htmlspecialchars($options['qtrans-switch-use-flags'], ENT_QUOTES);
+        $type = $options['qtrans-switch-type'];
+        if($type!='text'&&$type!='image'&&$type!='both'&&$type!='dropdown') $type='text';
 
         // The HTML below is the control form for editing options.
         ?>
         <div>
             <label for="qtrans-switch-title" style="line-height:35px;display:block;"><?php _e('Title:'); ?> <input type="text" id="qtrans-switch-title" name="qtrans-switch-title" value="<?php echo $title; ?>" /></label>
             <label for="qtrans-switch-hide-title" style="line-height:35px;display:block;"><?php _e('Hide Title:'); ?> <input type="checkbox" id="qtrans-switch-hide-title" name="qtrans-switch-hide-title" <?php echo ($hide_title=='on')?'checked="checked"':''; ?>/></label>
-            <label for="qtrans-switch-use-flags" style="line-height:35px;display:block;"><?php _e('Use Flags:'); ?> <input type="checkbox" id="qtrans-switch-use-flags" name="qtrans-switch-use-flags" <?php echo ($use_flags=='on')?'checked="checked"':''; ?>/></label>
+            <?php _e('Display:'); ?> <br />
+                <label for="qtrans-switch-type1"><input type="radio" name="qtrans-switch-type" id="qtrans-switch-type1" value="text"<?php echo ($type=='text')?' checked="checked"':'' ?>/><?php _e('Text only'); ?></label><br />
+                <label for="qtrans-switch-type2"><input type="radio" name="qtrans-switch-type" id="qtrans-switch-type2" value="image"<?php echo ($type=='image')?' checked="checked"':'' ?>/><?php _e('Image only'); ?></label><br />
+                <label for="qtrans-switch-type3"><input type="radio" name="qtrans-switch-type" id="qtrans-switch-type3" value="both"<?php echo ($type=='both')?' checked="checked"':'' ?>/><?php _e('Text and Image'); ?></label><br />
+                <label for="qtrans-switch-type4"><input type="radio" name="qtrans-switch-type" id="qtrans-switch-type4" value="dropdown"<?php echo ($type=='dropdown')?' checked="checked"':'' ?>/><?php _e('Dropdown Box'); ?></label><br />
             <input type="hidden" name="qtrans-switch-submit" id="qtrans-switch-submit" value="1" />
         </div>
         <?php
