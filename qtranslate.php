322d43b46329782d0efe33febccf3a827f55af71
chineseleper <chineseleper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-29 01:40:19 +0000

    qTranslate 2.0 beta initial checkin for wordpress 2.7 only basic admin language switcher design
    git-svn-id: http://plugins.svn.wordpress.org/qtranslate/trunk@71018 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- qtranslate.php
+++ qtranslate.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: qTranslate
 Plugin URI: http://www.qianqin.de/qtranslate/
-Description: Adds userfriendly multilingual content support into Wordpress. Inspired by <a href="http://fredfred.net/skriker/index.php/polyglot">Polyglot</a> from Martin Chlupac.
-Version: 1.1.4
+Description: Adds userfriendly multilingual content support into Wordpress. For Problems visit the <a href="http://www.qianqin.de/qtranslate/forum/">Support Forum</a>.
+Version: 2.0b
 Author: Qian Qin
 Author URI: http://www.qianqin.de
 Tags: multilingual, multi, language, admin, tinymce, qTranslate, Polyglot, bilingual, widget, switcher
@@ -41,94 +41,85 @@ Tags: multilingual, multi, language, admin, tinymce, qTranslate, Polyglot, bilin
     
 */
 
+// qTranslate Editor will only activated for the given version of Wordpress.
+// Can be changed to use with other versions but might cause problems or data loss!
+define('QT_SUPPORTED_WP_VERSION','2.7-almost-beta-9300');
+
 /* DEFAULT CONFIGURATION PART BEGINS HERE */
 
 // enable the use of following languages
-$q_config['enabled_languages'] = array('en', 'de', 'zh');
+$qt_config['enabled_languages'] = array('en', 'de', 'zh');
 
 // sets default language
-$q_config['default_language'] = 'en';
+$qt_config['default_language'] = 'en';
 
 // Names for languages in the corresponding language, add more if needed
-$q_config['language_name']['de'] = "Deutsch";
-$q_config['language_name']['en'] = "English";
-$q_config['language_name']['zh'] = "中文";
-$q_config['language_name']['fi'] = "suomi";
-$q_config['language_name']['fr'] = "Français";
-$q_config['language_name']['nl'] = "Nederlands";
-$q_config['language_name']['se'] = "Svenska";
-$q_config['language_name']['it'] = "Italiano";
+$qt_config['language_name']['de'] = "Deutsch";
+$qt_config['language_name']['en'] = "English";
+$qt_config['language_name']['zh'] = "中文";
+$qt_config['language_name']['fi'] = "suomi";
+$qt_config['language_name']['fr'] = "Français";
+$qt_config['language_name']['nl'] = "Nederlands";
+$qt_config['language_name']['se'] = "Svenska";
+$qt_config['language_name']['it'] = "Italiano";
 
 // Locales for languages
 // see locale -a for available locales
-$q_config['locale']['de'] = "de_DE";
-$q_config['locale']['en'] = "en_US";
-$q_config['locale']['zh'] = "zh_CN";
-$q_config['locale']['fi'] = "fi_FI";
-$q_config['locale']['fr'] = "fr_FR";
-$q_config['locale']['nl'] = "nl_NL";
-$q_config['locale']['se'] = "sv_SE";
-$q_config['locale']['it'] = "it_IT";
+$qt_config['locale']['de'] = "de_DE";
+$qt_config['locale']['en'] = "en_US";
+$qt_config['locale']['zh'] = "zh_CN";
+$qt_config['locale']['fi'] = "fi_FI";
+$qt_config['locale']['fr'] = "fr_FR";
+$qt_config['locale']['nl'] = "nl_NL";
+$qt_config['locale']['se'] = "sv_SE";
+$qt_config['locale']['it'] = "it_IT";
 
 // Language not available messages
 // %LANG:<normal_seperator>:<last_seperator>% generates a list of languages seperated by <normal_seperator> except for the last one, where <last_seperator> will be used instead.
-$q_config['not_available']['de'] = "Leider ist der Eintrag nur auf %LANG:, : und % verfügbar.";
-$q_config['not_available']['en'] = "Sorry, this entry is only available in %LANG:, : and %.";
-$q_config['not_available']['zh'] = "对不起，此内容只适用于%LANG:，:和%。";
-$q_config['not_available']['fi'] = "Anteeksi, mutta tämä kirjoitus on saatavana ainoastaan näillä kielillä: %LANG:, : ja %.";
-$q_config['not_available']['fr'] = "Désolé, cet article est seulement disponible en %LANG:, : et %.";
-$q_config['not_available']['nl'] = "Onze verontschuldigingen, dit bericht is alleen beschikbaar in %LANG:, : en %.";
-$q_config['not_available']['se'] = "Tyvärr är denna artikel enbart tillgänglig på %LANG:, : och %.";
-$q_config['not_available']['it'] = "Ci spiace, ma questo articolo è disponibile soltanto in %LANG:, : e %.";
-
-// enable strftime usage
-$q_config['use_strftime'] = true;
-
-// Date Configuration (uses strftime)
-$q_config['date_format']['en'] = '%A %B %e%q, %Y';
-$q_config['date_format']['de'] = '%A, der %e. %B %Y';
-$q_config['date_format']['zh'] = '%x %A';
-$q_config['date_format']['fi'] = '%e.&m.%C';
-$q_config['date_format']['fr'] = '%A %e %B %Y';
-$q_config['date_format']['nl'] = '%d/%m/%y';
-$q_config['date_format']['se'] = '%d/%m/%y';
-$q_config['date_format']['it'] = '%e %B %Y';
-
-$q_config['time_format']['en'] = '%I:%M %p';
-$q_config['time_format']['de'] = '%H:%M';
-$q_config['time_format']['zh'] = '%I:%M%p';
-$q_config['time_format']['fi'] = '%H:%M';
-$q_config['time_format']['fr'] = '%H:%M';
-$q_config['time_format']['nl'] = '%H:%M';
-$q_config['time_format']['se'] = '%H:%M';
-$q_config['time_format']['it'] = '%H:%M';
+$qt_config['not_available']['de'] = "Leider ist der Eintrag nur auf %LANG:, : und % verfügbar.";
+$qt_config['not_available']['en'] = "Sorry, this entry is only available in %LANG:, : and %.";
+$qt_config['not_available']['zh'] = "对不起，此内容只适用于%LANG:，:和%。";
+$qt_config['not_available']['fi'] = "Anteeksi, mutta tämä kirjoitus on saatavana ainoastaan näillä kielillä: %LANG:, : ja %.";
+$qt_config['not_available']['fr'] = "Désolé, cet article est seulement disponible en %LANG:, : et %.";
+$qt_config['not_available']['nl'] = "Onze verontschuldigingen, dit bericht is alleen beschikbaar in %LANG:, : en %.";
+$qt_config['not_available']['se'] = "Tyvärr är denna artikel enbart tillgänglig på %LANG:, : och %.";
+$qt_config['not_available']['it'] = "Ci spiace, ma questo articolo è disponibile soltanto in %LANG:, : e %.";
 
 // Flag images configuration
 // Look in /flags/ directory for a huge list of flags for usage
-$q_config['flag']['en'] = 'gb.png';
-$q_config['flag']['de'] = 'de.png';
-$q_config['flag']['zh'] = 'cn.png';
-$q_config['flag']['fi'] = 'fi.png';
-$q_config['flag']['fr'] = 'fr.png';
-$q_config['flag']['nl'] = 'nl.png';
-$q_config['flag']['se'] = 'se.png';
-$q_config['flag']['it'] = 'it.png';
+$qt_config['flag']['en'] = 'gb.png';
+$qt_config['flag']['de'] = 'de.png';
+$qt_config['flag']['zh'] = 'cn.png';
+$qt_config['flag']['fi'] = 'fi.png';
+$qt_config['flag']['fr'] = 'fr.png';
+$qt_config['flag']['nl'] = 'nl.png';
+$qt_config['flag']['se'] = 'se.png';
+$qt_config['flag']['it'] = 'it.png';
 
 // Location of flags (needs trailing slash!)
-$q_config['flag_location'] = 'wp-content/plugins/qtranslate/flags/';
+$qt_config['flag_location'] = 'wp-content/plugins/qtranslate/flags/';
 
 // Don't convert URLs to this file types
-$q_config['ignore_file_types'] = 'gif,jpg,jpeg,png,pdf,swf,tif,rar,zip,7z,mpg,divx,mpeg,avi,css,js';
+$qt_config['ignore_file_types'] = 'gif,jpg,jpeg,png,pdf,swf,tif,rar,zip,7z,mpg,divx,mpeg,avi,css,js';
 
 /* DEFAULT CONFIGURATION PART ENDS HERE */
 
-// Load qTranslate
-require_once("qtranslate_javascript.php");
-require_once("qtranslate_utils.php");
-require_once("qtranslate_core.php");
-require_once("qtranslate_wphacks.php");
-require_once("qtranslate_widget.php");
-require_once("qtranslate_configuration.php");
-require_once("qtranslate_hooks.php");
+function qt_admin_langselect() {
+    global $menu, $submenu, $qt_config;
+    
+    foreach($qt_config['enabled_languages'] as $id => $language) {
+        $class = '';
+        if(0==$id) $class = 'menu-top-first';
+        if(sizeof($qt_config['enabled_languages'])==$id+1) $class = 'menu-top-last';
+        $menu[] = array(__($qt_config['language_name'][$language]), 'read', '?lang='.$language, '', $class, 'menu-language-'.$language, get_option('home').'/'.$qt_config['flag_location'].$qt_config['flag'][$language]);
+        $submenu['?lang='.$language][] = array(sprintf(__('Switch to %s'), __($qt_config['language_name'][$language])), 'read', '?lang='.$language);
+    }
+}
+
+function qt_test(){
+    echo "blub";
+}
+
+add_action('_admin_menu',                    'qt_admin_langselect');
 
 ?>
