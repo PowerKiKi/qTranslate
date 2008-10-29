b6fde185ba9a9a3d0e792ed478c15cd9e0c82d18
chineseleper <chineseleper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-29 21:16:44 +0000

    admin language switcher for wp 2.7 running
    git-svn-id: http://plugins.svn.wordpress.org/qtranslate/trunk@71113 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- qtranslate.php
+++ qtranslate.php
@@ -9,46 +9,49 @@ Author URI: http://www.qianqin.de
 Tags: multilingual, multi, language, admin, tinymce, qTranslate, Polyglot, bilingual, widget, switcher
 */
 /*
-    Flags in flags directory are made by Luc Balemans and downloaded from
-    FOTW Flags Of The World website at http://flagspot.net/flags/
-    (http://www.crwflags.com/FOTW/FLAGS/wflags.html)
+	Flags in flags directory are made by Luc Balemans and downloaded from
+	FOTW Flags Of The World website at http://flagspot.net/flags/
+	(http://www.crwflags.com/FOTW/FLAGS/wflags.html)
 */
 /*  Copyright 2008  Qian Qin  (email : mail@qianqin.de)
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 /*
-    Default Language Contributers
-    en, de by Qian Qin
-    zh by Junyan Chen
-    fi by Tatu Siltanen
-    fr by Damien Choizit
-    nl by RobV
-    se by bear3556
-    it by Lorenzo De Tomasi
-    
+	Language Contributers
+	=====================
+	
+	en by Qian Qin
+	de by Qian Qin
+	zh by Junyan Chen
+	fi by Tatu Siltanen
+	fr by Damien Choizit
+	nl by RobV
+	se by bear3556
+	it by Lorenzo De Tomasi
+	
 */
 
 // qTranslate Editor will only activated for the given version of Wordpress.
 // Can be changed to use with other versions but might cause problems or data loss!
-define('QT_SUPPORTED_WP_VERSION','2.7-almost-beta-9300');
+define('QT_SUPPORTED_WP_VERSION', '2.7-almost-beta-9300');
 
 /* DEFAULT CONFIGURATION PART BEGINS HERE */
 
 // enable the use of following languages
-$qt_config['enabled_languages'] = array('en', 'de', 'zh');
+$qt_config['enabled_languages'] = array('de', 'en', 'zh');
 
 // sets default language
 $qt_config['default_language'] = 'en';
@@ -103,23 +106,74 @@ $qt_config['flag_location'] = 'wp-content/plugins/qtranslate/flags/';
 $qt_config['ignore_file_types'] = 'gif,jpg,jpeg,png,pdf,swf,tif,rar,zip,7z,mpg,divx,mpeg,avi,css,js';
 
 /* DEFAULT CONFIGURATION PART ENDS HERE */
+   
+function qt_init() {
+	global $qt_config, $qt_state;
+	
+	// prevent multiple inits 
+	if(defined('QTRANS_INIT')) return;
+	define('QTRANS_INIT',true);
+	
+	// Load config
+	$qt_config_load = get_option('qtranslate_configuration');
+	if(is_array($qt_config_load)) $qt_config = $qt_config_load;
+	
+	// check cookies
+	if(defined('WP_ADMIN') && !empty($_COOKIE['qt_admin_language']) && in_array($_COOKIE['qt_admin_language'], $qt_config['enabled_languages'])) {
+		$qt_state['language'] = $_COOKIE['qt_admin_language'];
+	} else {
+		$qt_state['language'] = $qt_config['default_language'];
+	}
+	
+	// Handling
+	$request_uri = $_SERVER['REQUEST_URI'];
+	$permalink_structure = get_option('permalink_structure');
+	if(preg_match('#%LANG%#i',$permalink_structure)) {
+		// pretty URLs
+	}
+	if(!empty($_GET['lang'])) {
+		// language modifier detected
+		if(in_array($_GET['lang'], $qt_config['enabled_languages'])) {
+			$qt_state['language'] = $_GET['lang'];
+			if(defined('WP_ADMIN'))
+				setcookie('qt_admin_language', $qt_state['language'], time()+60*60*24*30);
+			}
+	}
+}
+
+function qt_get_locale($locale) {
+	global $qt_config, $qt_state;
+	qt_init();
+	
+	$locale = array();
+	$locale[] = $qt_config['locale'][$qt_state['language']].".utf8";
+	$locale[] = $qt_config['locale'][$qt_state['language']]."@euro";
+	$locale[] = $qt_config['locale'][$qt_state['language']];
+	$locale[] = $qt_config['language'];
+	// return the correct locale and most importantly set it (wordpress doesn't, which is bad)
+	setlocale(LC_ALL, $locale);
+	return $qt_config['locale'][$qt_state['language']];
+}
 
 function qt_admin_langselect() {
-    global $menu, $submenu, $qt_config;
-    
-    foreach($qt_config['enabled_languages'] as $id => $language) {
-        $class = '';
-        if(0==$id) $class = 'menu-top-first';
-        if(sizeof($qt_config['enabled_languages'])==$id+1) $class = 'menu-top-last';
-        $menu[] = array(__($qt_config['language_name'][$language]), 'read', '?lang='.$language, '', $class, 'menu-language-'.$language, get_option('home').'/'.$qt_config['flag_location'].$qt_config['flag'][$language]);
-        $submenu['?lang='.$language][] = array(sprintf(__('Switch to %s'), __($qt_config['language_name'][$language])), 'read', '?lang='.$language);
-    }
+	global $menu, $submenu, $qt_config;
+	
+	// don't display menu if there is only 1 language active
+	if(sizeof($qt_config['enabled_languages']) <= 1) return;
+	
+	// generate menu with flags for every enabled language
+	foreach($qt_config['enabled_languages'] as $id => $language) {
+		$class = '';
+		if(0 == $id) $class = 'menu-top-first';
+		if(sizeof($qt_config['enabled_languages']) == $id + 1) $class = 'menu-top-last';
+		$menu[] = array(__($qt_config['language_name'][$language]), 'read', '?lang='.$language, '', $class, 'menu-language-'.$language, get_option('home').'/'.$qt_config['flag_location'].$qt_config['flag'][$language]);
+		$submenu['?lang='.$language][] = array(sprintf(__('Switch to %s'), __($qt_config['language_name'][$language])), 'read', '?lang='.$language);
+	}
 }
 
-function qt_test(){
-    echo "blub";
-}
 
-add_action('_admin_menu',                    'qt_admin_langselect');
+add_action('_admin_menu',	'qt_admin_langselect');
+
+add_filter('locale',	'qt_get_locale',99);
 
 ?>
