0e3eb8d1929dc5503bb617928515141f56c38218
chineseleper <chineseleper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-06 02:31:55 +0000

    wp 2.8.3 compatibility
    git-svn-id: http://plugins.svn.wordpress.org/qtranslate/trunk@143220 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- qtranslate.php
+++ qtranslate.php
@@ -3,7 +3,7 @@
 Plugin Name: qTranslate
 Plugin URI: http://www.qianqin.de/qtranslate/
 Description: Adds userfriendly multilingual content support into Wordpress. For Problems visit the <a href="http://www.qianqin.de/qtranslate/forum/">Support Forum</a>.
-Version: 2.4.1
+Version: 2.4.2
 Author: Qian Qin
 Author URI: http://www.qianqin.de
 Tags: multilingual, multi, language, admin, tinymce, qTranslate, Polyglot, bilingual, widget, switcher, professional, human, translation, service
@@ -61,7 +61,7 @@ Tags: multilingual, multi, language, admin, tinymce, qTranslate, Polyglot, bilin
 
 // qTranslate Editor will only activated for the given version of Wordpress.
 // Can be changed to use with other versions but might cause problems and/or data loss!
-define('QT_SUPPORTED_WP_VERSION', '2.8.2');
+define('QT_SUPPORTED_WP_VERSION', '2.8.3');
 define('QT_STRING',		1);
 define('QT_BOOLEAN',	2);
 define('QT_INTEGER',	3);
