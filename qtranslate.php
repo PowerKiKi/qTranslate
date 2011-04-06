4c1e80089bf7d1c581a44a782e45f389c2e3c52d
chineseleper <chineseleper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-06 11:44:57 +0000

    wordpress 3.1.1 change
    git-svn-id: http://plugins.svn.wordpress.org/qtranslate/trunk@369449 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- qtranslate.php
+++ qtranslate.php
@@ -79,7 +79,7 @@ Tags: multilingual, multi, language, admin, tinymce, qTranslate, Polyglot, bilin
 
 // qTranslate Editor will only activated for the given version of Wordpress.
 // Can be changed to use with other versions but might cause problems and/or data loss!
-define('QT_SUPPORTED_WP_VERSION', '3.1');
+define('QT_SUPPORTED_WP_VERSION', '3.1.1');
 define('QT_STRING',		1);
 define('QT_BOOLEAN',	2);
 define('QT_INTEGER',	3);
